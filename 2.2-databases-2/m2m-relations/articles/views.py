411b314e279760e5d0423d327c1d7e6c68976189
MaximShaybakov <momon_boton@mail.ru>
2022-08-30 01:51:51 +0500

    Done

--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -10,5 +10,7 @@ def articles_list(request):
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = '-published_at'
-
+    article = Article.objects.all()
+    context = {'object_list': article}
+    print()
     return render(request, template, context)
