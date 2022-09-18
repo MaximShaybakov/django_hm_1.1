8eb4c2b991aec881422ceb7ac865a11f5487789f
MaximShaybakov <momon_boton@mail.ru>
2022-09-18 10:19:54 +0500

    Done.

--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -7,17 +7,27 @@ from students.models import Student, Course
 
 @pytest.fixture
 def client():
+    '''fixture for api-client'''
     return APIClient()
 
 
 @pytest.fixture
 def students(client):
+    '''fixture for students factory'''
+    response = client.get('/courses/')
+    return response.json()
+
+
+@pytest.fixture
+def students(client):
+    '''fixture for students'''
     response = client.get('/courses/')
     return response.json()
 
 
 @pytest.fixture
 def courses(client):
+    '''fixture for students factory'''
     def courses_get(client, *args, **kwargs):
         response = client.get('/courses/')
         return response.json()
@@ -26,6 +36,7 @@ def courses(client):
 
 @pytest.fixture
 def courses_factory():
+    '''fixture for create students'''
     def factory(*args, **kwargs):
         return baker.make(Course, *args, **kwargs)
     return factory
@@ -33,50 +44,70 @@ def courses_factory():
 
 @pytest.mark.django_db
 def test_api(client):
+    '''server response test'''
     response = client.get('/courses/')
     assert response.status_code == 200
 
 
-@pytest.mark.django_db # проверка получения 1го курса (retrieve-логика)
+@pytest.mark.django_db
 def test_get_course(client, courses, courses_factory):
+    '''receiving 1 course'''
     course = courses_factory(_quantity=1)
     response = client.get('/courses/1/')
+    assert response.status_code == 200
     assert course[0].name == response.json()['name']
     assert course[0].id == 1
 
 
 @pytest.mark.django_db
 def test_create_course(client, courses, courses_factory):
+    '''course creation test'''
     course = courses_factory(_quantity=10)
     response = client.get('/courses/')
+    data = response.json()
+    assert response.status_code == 200
+    assert isinstance(data, (list, ))
     assert len(course) == len(courses(client))
 
 
-@pytest.mark.django_db # доделать!!!
+@pytest.mark.django_db
 def test_get_list_course(client, courses, courses_factory):
+    '''creating and checking a list of courses'''
     course = courses_factory(_quantity=10)
     response = client.get('/courses/')
-    for index, course_ in response:
-        assert course_.json()[index]['name'] == course[index].name
+    data = response.json()
+    assert response.status_code == 200
+    assert len(course) == len(data)
+    assert isinstance(data, (list, ))
+    for index, course_ in enumerate(data):
+        assert course[index].name == course_['name']
 
 
 @pytest.mark.django_db
 def test_create_one_course(client):
+    '''create new course test'''
     response = client.post('/courses/', data={'name': 'music', 'students': []})
     assert response.status_code == 201
 
 
-@pytest.mark.django_db
-def test_update_course(client, courses, courses_factory):
+@pytest.mark.django_db # работает при запуске отдельно
+def test_update_course(client, courses_factory):
+    '''course update test'''
+    name = 'music'
     course = courses_factory(_quantity=1)
-    response = client.patch('/courses/', data={'name': 'music', 'students': []})
-    assert response.status_code == 201
+    response = client.patch('/courses/1/', data={'name': name, 'students': []})
+    new_name = client.get('/courses/1/').json()
+    assert response.status_code == 200
+    assert new_name['name'] == name
 
 
-@pytest.mark.django_db
+@pytest.mark.django_db # работает при запуске отдельно
 def test_rm_course(client, courses, courses_factory):
-    course = courses_factory(_quantity=1)
+    '''course deletion test'''
+    course = courses_factory(_quantity=10)
     response = client.delete('/courses/1/')
+    new_count_courses = client.get('/courses/').json()
     assert response.status_code == 204
+    assert len(new_count_courses) == len(course) - 1
 
 
